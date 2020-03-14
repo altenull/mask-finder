27fb297696dea5695a2bec6116137e1790f469b2
Heonyoung Kim <altenull@gmail.com>
2020-03-14 18:51:24 +0900

    chore: add styled-components module and configure it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import { fontWeights } from './app/src/ui/inline-styles';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,500');
+
+  html, body {
+    margin: 0;
+    font-family: 'Roboto', Arial, sans-serif;
+    font-weight: ${fontWeights.ultraLight};
+    font-size: 16px;
+    height: 100%;
+    min-height: 100%;
+    box-sizing: border-box;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    user-select: none;
+  }
+
+  #root {
+    height: 100%;
+  }
+`;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
---
+++ src/app/src/ui/inline-styles/variables.ts
@@ -0,0 +1,11 @@
+export const fontWeights = {
+  thin: 100,
+  ultraLight: 200,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  heavy: 800,
+  black: 900
+};
