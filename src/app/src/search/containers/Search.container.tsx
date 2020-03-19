2939ebc90569d5cf6a1cb68f4acdb801e02eb0c8
Heonyoung Kim <altenull@gmail.com>
2020-03-20 08:19:57 +0900

    fix(search): prevent to request keywordSearch if the keyword is empty

--- src/app/src/search/containers/Search.container.tsx
+++ src/app/src/search/containers/Search.container.tsx
@@ -9,19 +9,19 @@ export const SearchContainer: React.FC = () => {
   const [keyword, setKeyword] = useState<string>('');
   const { updateMapCoordinates }: MapContextState = useContext(MapContext);
 
+  const places = new window.kakao.maps.services.Places();
+
   useEffect(() => {
-    const places = new window.kakao.maps.services.Places();
-
-    const callback = (result: Place[], status: any) => {
-      if (status === window.kakao.maps.services.Status.OK) {
-        const placeVMs: PlaceVM[] = result.map((place: Place) => parsePlaceVMFromPlace(place));
-        if (placeVMs.length > 0) {
-          updateMapCoordinates(placeVMs[0].coordinates);
+    if (!!keyword) {
+      places.keywordSearch(keyword, (result: Place[], status: any) => {
+        if (status === window.kakao.maps.services.Status.OK) {
+          const placesVM: PlaceVM[] = result.map((place: Place) => parsePlaceVMFromPlace(place));
+          if (placesVM.length > 0) {
+            updateMapCoordinates(placesVM[0].coordinates);
+          }
         }
-      }
-    };
-
-    places.keywordSearch(keyword, callback);
+      });
+    }
   }, [keyword]);
 
   return <SearchInput setKeyword={setKeyword} />;
