0cd75d991da8963fbc9916aef6c4bd925adfe9b5
Heonyoung Kim <altenull@gmail.com>
2020-04-04 23:48:10 +0900

    feat(assets, map): add find route icon and apply it to mask store tooltip

--- src/app/src/map/utils/map.util.ts
+++ src/app/src/map/utils/map.util.ts
@@ -72,17 +72,18 @@ export const getMaskStoreTooltipContent = ({
     [RemainStatus.Empty]: '없음(1개 이하)',
   };
   const findRouteLink: string = `https://map.kakao.com/link/to/${name},${latitude},${longitude}`;
-  const closeIcon: string = `<svg stroke="#999999" fill="#999999" stroke-width="0" viewBox="0 0 24 24" width="28" height="28" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>`;
 
   return `
     <div class="mask-store-tooltip">
       <div class="mask-store-tooltip__top-group">
         <i class="mask-store-tooltip__close-icon" onclick="removeMaskStoreTooltip()" >
-          ${closeIcon}
+          <svg stroke="#999999" fill="#999999" stroke-width="0" viewBox="0 0 24 24" width="28" height="28" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
         </i>
         <h2 class="mask-store-tooltip__name">${name}</h2>
         <p class="mask-store-tooltip__address">${roadAddress}</p>
-        <a class="mask-store-tooltip__find-route" href="${findRouteLink}" rel="noopener" target="_blank">길찾기</a>
+        <a class="mask-store-tooltip__find-route" href="${findRouteLink}" rel="noopener" target="_blank">
+          <svg stroke="#ffffff" fill="#ffffff" stroke-width="0" viewBox="0 0 512 512" height="16" width="16" xmlns="http://www.w3.org/2000/svg"><path d="M416 320h-96c-17.6 0-32-14.4-32-32s14.4-32 32-32h96s96-107 96-160-43-96-96-96-96 43-96 96c0 25.5 22.2 63.4 45.3 96H320c-52.9 0-96 43.1-96 96s43.1 96 96 96h96c17.6 0 32 14.4 32 32s-14.4 32-32 32H185.5c-16 24.8-33.8 47.7-47.3 64H416c52.9 0 96-43.1 96-96s-43.1-96-96-96zm0-256c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zM96 256c-53 0-96 43-96 96s96 160 96 160 96-107 96-160-43-96-96-96zm0 128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg>
+        </a>
       </div>
       <div class="mask-store-tooltip__diagonal-divider"></div>
       <div class="mask-store-tooltip__bottom-group">
