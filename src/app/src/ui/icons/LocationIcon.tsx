aa2621f5fdbe47ca2b0b9fe2f45c7c046eed80bf
Heonyoung Kim <altenull@gmail.com>
2020-04-05 01:14:24 +0900

    fix(ui): change stroke-width to strokeWidth

--- src/app/src/ui/icons/LocationIcon.tsx
+++ src/app/src/ui/icons/LocationIcon.tsx
@@ -7,7 +7,7 @@ export const LocationIcon: React.FC = () => {
     <svg
       stroke={inlineColors.gray4}
       fill={inlineColors.gray4}
-      stroke-width='0'
+      strokeWidth='0'
       viewBox='0 0 24 24'
       height='20'
       width='20'
