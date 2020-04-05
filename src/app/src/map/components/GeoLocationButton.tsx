b9d5a350ddd2cf995f6cbf2633c1a481e07b22bf
Heonyoung Kim <altenull@gmail.com>
2020-04-05 14:29:56 +0900

    style(assets, map, ui): add box-shadow styles and apply it to geoLocation button, mask store tooltip

--- src/app/src/map/components/GeoLocationButton.tsx
+++ src/app/src/map/components/GeoLocationButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { LocationIcon } from '../../ui/icons';
+import { inlineStyles } from '../../ui/inline-styles';
 
 interface GeoLocationButtonProps {
   className?: string;
@@ -16,7 +17,7 @@ const StdButtonWrapper = styled.button`
   outline: none;
   border: none;
   cursor: pointer;
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  box-shadow: ${inlineStyles.boxShadow.boxShadow2};
 `;
 
 export const GeoLocationButton: React.FC<GeoLocationButtonProps> = ({
