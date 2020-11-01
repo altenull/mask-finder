58800adc1b352d2a5f30c4a3f437c96fb9471df5
Heonyoung Kim <altenull@gmail.com>
2020-11-01 23:21:53 +0900

    feat(mask-finder-api): prevent to request get mask stores api

--- src/app/src/mask-finder-api/hooks/mask-store.hook.ts
+++ src/app/src/mask-finder-api/hooks/mask-store.hook.ts
@@ -1,9 +1,12 @@
-import { useEffect, useState, useCallback } from 'react';
-
+import { useCallback, useEffect, useState } from 'react';
+import { MapCoordinates } from '../../map/models/map';
 import { MaskStoreVM } from '../models/mask-store';
 import { maskStoreService } from '../services/mask-store.service';
-import { MapCoordinates } from '../../map/models/map';
 
+/**
+ * 공적마스크 판매 중단에 따른 공공데이터포털내 오픈 API 서비스 종료 (2020-07-08)
+ * https://www.data.go.kr/bbs/ntc/selectNotice.do?originId=NOTICE_0000000001728
+ */
 export const useGetMaskStores = (mapCoordinates: MapCoordinates) => {
   const [maskStores, setMaskStores] = useState<MaskStoreVM[]>([]);
   const [isGetMaskStoresLoading, setIsGetMaskStoresLoading] = useState<boolean>(false);
@@ -29,7 +32,7 @@ export const useGetMaskStores = (mapCoordinates: MapCoordinates) => {
   }, [mapCoordinates]);
 
   useEffect(() => {
-    getFetchData();
+    // getFetchData();
   }, [getFetchData]);
 
   return { maskStores, isGetMaskStoresLoading, getMaskStoresError };
